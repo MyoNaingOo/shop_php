90eed11f90508b72e7e5a4b9377773c2ee686ce2
MyoNaingOo <104154279+MyoNaingOo@users.noreply.github.com>
2022-10-02 04:29:35 +0630

    Helper

---
+++ contect.php
@@ -0,0 +1,26 @@
+<div class="col-sm-6 center ">
+<img src="imgs/logo.jpg" class="logoimg"/>
+<p>Product From MNOIT</br>DELOPER Myo Naing Oo</p>
+</br>
+</div>
+
+<div class="col-sm-6 mt-3 center">
+<a href="#" class="nav-link mx-1">
+<i class="fab fa-facebook text-primary"></i><span class="d-none d-md-inline">Facebook</span>
+</a>
+<a href="#" class="nav-link mx-1">
+<i class="fab fa-youtube text-danger"></i><span class="d-none d-md-inline" >You Tube</span>
+</a>
+<a href="#" class="nav-link mx-1">
+<i class="fab fa-twitter text-blue"></i><span class="d-none d-md-inline">Twitter</span>
+</a>
+<a href="#" class="nav-link mx-1">
+<i class="fab fa-instagram instagram"></i><span class="d-none d-md-inline">Instagram</span>
+</a>
+<a href="#" class="nav-link mx-1">
+<i class="fab fa-github"></i><span class="d-none d-md-inline">Git Hut</span>
+</a>
+</div>
+
+</div>
+<footer class="p-2 center bg-primary ">&copy 2022 MNO</footer>
